814443f4e147c24d57b57943525fdf7b844d981c
SWAROOP Shivanna <swarooprooney@gmail.com>
2019-11-15 22:40:45 +0000

    Adding Login View
    Adding login shell view

--- TransferTracker/Bootstrapper.cs
+++ TransferTracker/Bootstrapper.cs
@@ -5,20 +5,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using TransferTracker.Helper;
 using TransferTracker.ViewModels;
 
 namespace TransferTracker
 {
 	public class Bootstrapper :BootstrapperBase
 	{
+		private SimpleContainer _container = new SimpleContainer();
 		public Bootstrapper()
 		{
 			Initialize();
+			ConventionManager.AddElementConvention<PasswordBox>(
+		   PasswordBoxHelper.BoundPasswordProperty,
+		   "Password",
+		   "PasswordChanged");
 		}
 
 		protected override void OnStartup(object sender, StartupEventArgs e)
 		{
 			DisplayRootViewFor<ShellViewModel>();
+		}
+
+		protected override void Configure()
+		{
+			_container.Instance(_container);
+			_container
+				.Singleton<IWindowManager, WindowManager>()
+				.Singleton<IEventAggregator, EventAggregator>();
+
+			GetType().Assembly.GetTypes()
+				.Where(type => type.IsClass)
+				.Where(type => type.Name.EndsWith("ViewModel"))
+				.ToList()
+				.ForEach(viewModelType => _container.RegisterPerRequest(
+					viewModelType, viewModelType.ToString(), viewModelType));
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service)
+		{
+			return _container.GetAllInstances(service);
+		}
+
+		protected override object GetInstance(Type service, string key)
+		{
+			return _container.GetInstance(service, key);
 		}
 	}
 }
---
+++ TransferTracker/ViewModels/LoginViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransferTracker.ViewModels
+{
+	public class LoginViewModel : Screen
+	{
+		private string _userName;
+		private string _password;
+
+		public string UserName
+		{
+			get { return _userName; }
+			set
+			{
+				_userName = value;
+				NotifyOfPropertyChange(() => UserName);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public string Password
+		{
+			get { return _password; }
+			set
+			{
+				_password = value;
+				NotifyOfPropertyChange(() => Password);
+				NotifyOfPropertyChange(() => CanLogIn);
+			}
+		}
+
+		public bool CanLogIn
+		{
+			get
+			{
+				bool result = false;
+				if (_userName?.Length > 0 && _password?.Length > 0)
+				{
+					result = true;
+				}
+				return result;
+			}
+		}
+
+		public void LogIn()
+		{
+			Console.WriteLine();
+		}
+
+	}
+}
